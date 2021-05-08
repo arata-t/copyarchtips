d188ebeb835d5a0bb7c7ce99bd110fdffa5a9858
arata-t <taniguro.arata@gmail.com>
2021-05-08 10:59:35 +0900

    モデルにデータベースを記述、mount_uploader　追記

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -2,6 +2,7 @@ class Tip < ApplicationRecord
   belongs_to :user
   has_many :comments
   # has_one_attached :image
+  mount_uploader :image, ImageUploader #carrierwave
   has_many :tip_tag_relations, foreign_key: :tip_id, dependent: :destroy
   has_many :tags, through: :tip_tag_relations
 
