5a1019651c306d4906412e05d7be0c4538fcad91
arata-t <taniguro.arata@gmail.com>
2021-05-08 16:43:39 +0900

    TEST factorybot image追加

--- spec/factories/tips.rb
+++ spec/factories/tips.rb
@@ -2,11 +2,8 @@ FactoryBot.define do
   factory :tip do
     title            { Faker::Lorem.sentence }
     category_id      { 2 }
+    image            { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test_image.png')) }
     description      { Faker::Lorem.sentence }
     association :user
-
-    after(:build) do |tip|
-      tip.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-    end
   end
 end
